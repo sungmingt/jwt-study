ef146a54e7ea877c5e4e6d0400eccf95a1da175f
sungmingt <qotjdals8313@naver.com>
2022-10-20 17:40:23 +0900

    FEAT : add blacklist logic

--- src/main/java/codestates/jwt/study/domain/util/JwtUtil.java
+++ src/main/java/codestates/jwt/study/domain/util/JwtUtil.java
@@ -4,13 +4,16 @@ import codestates.jwt.study.domain.redis.RedisUtil;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtUtil {
 
     private final RedisUtil redisUtil;
@@ -21,14 +24,14 @@ public class JwtUtil {
     public static final String PREFIX = "Bearer ";
     public static final String AUTHORIZATION = "Authorization";
 
-    public static final long ACCESS_TOKEN_VALIDATION_SECOND = 1000L * 10;
-    public static final long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 24 * 2;
+    public static final long ACCESS_TOKEN_VALIDATION_SECOND = 1000L * 60 * 30;  //30분
+    public static final long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 60 * 24 * 3;  //3일
 
     public static final String ACCESS_TOKEN_NAME = "accessToken";
     public static final String REFRESH_TOKEN_NAME = "refreshToken";
 
     public String createAccessToken(String email) {
-        return JWT.create()
+        return PREFIX + JWT.create()
                 .withSubject(SECRET_KEY)
                 .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDATION_SECOND))
                 .withClaim("email", email)
@@ -38,43 +41,59 @@ public class JwtUtil {
     public String createRefreshToken(String email) {
         return JWT.create()
                 .withSubject(SECRET_KEY)
-                .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_TOKEN_VALIDATION_SECOND))
+                .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_TOKEN_VALIDATION_SECOND))  ///////////////////////
                 .withClaim("email", email)
                 .sign(Algorithm.HMAC512(SECRET_KEY));
     }
 
-    public String verifyToken(String token) {
+    public Map<String, String> verifyToken(String token) {
+
+        log.info("### verifyToken 입장");
         //토큰 만료 시 refresh token 만료여부 조회
         if (isExpired(token)) {
+            log.info("### access token expired- {}", token);
+
             String email = getEmail(token);
+
+            log.info("### email - {}", email);
+
             String refreshToken = redisUtil.getData(email);
+
+            log.info("### refreshToken - {}", refreshToken);
 
             //refresh token 도 만료 시 강제 로그아웃
             if (refreshToken == null) {
+                log.info("### refresh token expired - {}", email);
+
                 throw new RuntimeException("refresh token 만료, 강제 로그아웃");
             }
 
             //access token 새로 발급
             String accessToken = createAccessToken(email);
-            //refresh token 만료시간 연장 (redis key값 덮어씌우기 되는지)
+            //기존 refresh token 만료시간 초기화
             redisUtil.setDataExpire(email, refreshToken, REFRESH_TOKEN_VALIDATION_SECOND);
 
-            return accessToken;
+            return Map.of(ACCESS_TOKEN_NAME, accessToken);
         }
 
-        return JWT.require(Algorithm.HMAC512(SECRET_KEY))
-                .build()
-                .verify(token)
-                .getClaim("email")
-                .asString();
+        return Map.of("email",
+                JWT.require(Algorithm.HMAC512(SECRET_KEY))
+                        .build()
+                        .verify(token)
+                        .getClaim("email")
+                        .asString());
     }
 
     private boolean isExpired(String token) {
         return JWT.decode(token).getExpiresAt().before(new Date());
     }
 
+    public Long getExpiration(String token) {
+        return JWT.decode(token).getExpiresAt().getTime() - System.currentTimeMillis();
+    }
+
     private String getEmail(String token) {
-        return JWT.decode(token).getClaim(token).asString();
+        return JWT.decode(token).getClaim("email").asString();
     }
 
 //    public Map<String,Object> getClaimsFromToken(String token, String keys){
--- src/main/java/codestates/jwt/study/web/filter/JwtAuthorizationFilter.java
+++ src/main/java/codestates/jwt/study/web/filter/JwtAuthorizationFilter.java
@@ -3,36 +3,33 @@ package codestates.jwt.study.web.filter;
 import codestates.jwt.study.domain.Member;
 import codestates.jwt.study.domain.oauth.PrincipalDetails;
 import codestates.jwt.study.domain.MemberRepository;
+import codestates.jwt.study.domain.redis.RedisUtil;
 import codestates.jwt.study.domain.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 import static codestates.jwt.study.domain.util.JwtUtil.*;
-//Security filter → 권한 및 인증이 필요한 주소를 요청 시 BasicAuthenticationFilter를 반드시 진행하게 되어있습니다.
-//권한이나 인증이 필요하지 않을 경우 BasicAuthenticationFilter는 적용되지 않습니다.
 
-//인증 권한이 필요한 url에 접속할 때 이 필터가 적용되도록 할 것이다.
 @Slf4j
-public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
-
-    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, MemberRepository memberRepository, JwtUtil jwtUtil) {
-        super(authenticationManager);
-        this.memberRepository = memberRepository;
-        this.jwtUtil = jwtUtil;
-    }
+    private final RedisUtil redisUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -40,24 +37,55 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
         String jwtHeader = request.getHeader(ACCESS_TOKEN_NAME);
 
+        log.info("### 헤더 확인");
         if (jwtHeader == null || !jwtHeader.startsWith(PREFIX)) {
             chain.doFilter(request, response);
             return;
         }
 
         String token = jwtHeader.replace(PREFIX, "");
-        String email = jwtUtil.verifyToken(token);
+        log.info("### 토큰 확인 - {}", token);
+
+        if (redisUtil.isBlacklist(token)) {
+            log.info("### access token from blacklist - {}", token);
+            throw new RuntimeException("사용할 수 없는 토큰입니다.");
+        }
+
+
+        Map<String, String> verified = jwtUtil.verifyToken(token);
+
+        log.info("### 토큰 유효 확인");
+
+        if (verified.containsKey(ACCESS_TOKEN_NAME)) {
+            response.addHeader(ACCESS_TOKEN_NAME, verified.get(ACCESS_TOKEN_NAME));
+            chain.doFilter(request, response);
+            return;
+            // 여기서 요청이 끝나기 때문에 클라이언트에서 새로 발급받은 access token을 받아 다시 요청을 보내야한다.
+        }
+
+        String email = verified.get("email");
 
         if (email != null) {
+            log.info("### 이메일 확인 - {}", email);
             Member memberEntity = memberRepository.findByEmail(email)
                     .orElseThrow(() -> new RuntimeException("존재하지 않는 이메일입니다."));
 
+            log.info("### 이메일 유효 확인");
             PrincipalDetails principalDetails = new PrincipalDetails(memberEntity);
-            Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails, null, principalDetails.getAuthorities());
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(principalDetails, null, principalDetails.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            log.info("### set context holder");
+
+            if (request.getRequestURI().equals("/custom/logout")) {
+                log.info("### filter - email ={}", email);
+                request.setAttribute("email", email);
+            }
 
             chain.doFilter(request, response);
         }
-        super.doFilterInternal(request, response, chain);
+        log.info("### 첫 do filter");
+        chain.doFilter(request, response);
     }
 }
