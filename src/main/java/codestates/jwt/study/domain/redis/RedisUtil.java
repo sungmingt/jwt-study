f8b890175c06f838ea455e8baac9157a8d64911c
sungmingt <qotjdals8313@naver.com>
2022-10-20 17:37:16 +0900

    FEAT : add blacklist logic / define static constant

--- src/main/java/codestates/jwt/study/domain/redis/RedisUtil.java
+++ src/main/java/codestates/jwt/study/domain/redis/RedisUtil.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class RedisUtil {
@@ -13,19 +14,21 @@ public class RedisUtil {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    private static final String BLACKLIST = "blacklist_";
+
     public String getData(String key){
         ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
         return valueOperations.get(key);
     }
 
     public void setData(String key, String value){
-        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
         valueOperations.set(key,value);
     }
 
-    public void setDataExpire(String key,String value,long duration){
+    public void setDataExpire(String key, String value, long duration){
         ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
-        Duration expireDuration = Duration.ofSeconds(duration);
+        Duration expireDuration = Duration.ofDays(3);
         valueOperations.set(key,value,expireDuration);
     }
 
@@ -33,4 +36,15 @@ public class RedisUtil {
         stringRedisTemplate.delete(key);
     }
 
+    public void setBlackList(String accessToken, Long time) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        valueOperations.set(BLACKLIST + accessToken, "logout", time, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean isBlacklist(String accessToken) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        String value = valueOperations.get(BLACKLIST + accessToken);
+
+        return value != null && !value.isEmpty();
+    }
 }
